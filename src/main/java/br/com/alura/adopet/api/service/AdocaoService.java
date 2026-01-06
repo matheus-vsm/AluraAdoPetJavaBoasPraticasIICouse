6d962174cfd3be1c0f21552c502d28c7ce21b732
matheus-vsm <scompinha@hotmail.com>
2026-01-05 23:33:05 -0300

    aula 3.3

--- src/main/java/br/com/alura/adopet/api/service/AdocaoService.java
+++ src/main/java/br/com/alura/adopet/api/service/AdocaoService.java
@@ -11,6 +11,7 @@ import br.com.alura.adopet.api.model.Tutor;
 import br.com.alura.adopet.api.repository.AdocaoRepository;
 import br.com.alura.adopet.api.repository.PetRepository;
 import br.com.alura.adopet.api.repository.TutorRepository;
+import br.com.alura.adopet.api.validacoes.ValidacaoSolicitacaoAdocao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -31,13 +32,18 @@ public class AdocaoService {
     private TutorRepository tutorRepository;
 
     @Autowired
+    private List<ValidacaoSolicitacaoAdocao> validadores;
+
+    @Autowired
     private EmailService emailService;
 
     public void solicitar(SolicitacaoAdocaoDto dto) {
         Pet pet = petRepository.getReferenceById(dto.idPet());
         Tutor tutor = tutorRepository.getReferenceById(dto.idTutor());
 
-
+        validadores.forEach(
+                v -> v.validar(dto)
+        );
 
         Adocao adocao = new Adocao();
         adocao.setData(LocalDateTime.now());
