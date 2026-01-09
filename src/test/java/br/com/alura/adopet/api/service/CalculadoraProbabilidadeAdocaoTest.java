c1d2e54d7935998b2a05ad3d86383ada58f9cfbf
matheus-vsm <scompinha@hotmail.com>
2026-01-08 22:59:15 -0300

    aula 1.4

---
+++ src/test/java/br/com/alura/adopet/api/service/CalculadoraProbabilidadeAdocaoTest.java
@@ -0,0 +1,14 @@
+package br.com.alura.adopet.api.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculadoraProbabilidadeAdocaoTest {
+
+    @Test
+    void testCalculaProbabilidadeAdocao() {
+        // Implementar testes unitários para o método calcular da classe CalculadoraProbabilidadeAdocao
+    }
+
+}
