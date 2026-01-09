00db434703889bcd2d981b090a093508296447a9
matheus-vsm <scompinha@hotmail.com>
2026-01-08 23:05:35 -0300

    aula 1.5

--- src/test/java/br/com/alura/adopet/api/service/CalculadoraProbabilidadeAdocaoTest.java
+++ src/test/java/br/com/alura/adopet/api/service/CalculadoraProbabilidadeAdocaoTest.java
@@ -1,14 +1,38 @@
 package br.com.alura.adopet.api.service;
 
+import br.com.alura.adopet.api.dto.abrigo.CadastroAbrigoDto;
+import br.com.alura.adopet.api.dto.abrigo.CadastroPetDto;
+import br.com.alura.adopet.api.model.Abrigo;
+import br.com.alura.adopet.api.model.Pet;
+import br.com.alura.adopet.api.model.ProbabilidadeAdocao;
+import br.com.alura.adopet.api.model.TipoPet;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class CalculadoraProbabilidadeAdocaoTest {
 
     @Test
     void testCalculaProbabilidadeAdocao() {
-        // Implementar testes unitários para o método calcular da classe CalculadoraProbabilidadeAdocao
+        //idade 4 anos e 4kg - ALTA
+
+        Abrigo abrigo = new Abrigo(new CadastroAbrigoDto(
+                "Abrigo feliz",
+                "94999999999",
+                "abrigofeliz@email.com.br"
+        ));
+        Pet pet = new Pet(new CadastroPetDto(
+                TipoPet.GATO,
+                "Miau",
+                "Siames",
+                4,
+                "Cinza",
+                4.0f
+        ), abrigo);
+
+        CalculadoraProbabilidadeAdocao calculadora = new CalculadoraProbabilidadeAdocao();
+        ProbabilidadeAdocao probabilidade = calculadora.calcular(pet);
+
+        Assertions.assertEquals(ProbabilidadeAdocao.ALTA, probabilidade);
     }
 
 }
