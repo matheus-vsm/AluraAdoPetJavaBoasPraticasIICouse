80ae15324fcc842ebdfb43e03eec478bba95c897
matheus-vsm <scompinha@hotmail.com>
2026-01-11 23:40:19 -0300

    aula 4.3

--- src/test/java/br/com/alura/adopet/api/service/AdocaoServiceTest.java
+++ src/test/java/br/com/alura/adopet/api/service/AdocaoServiceTest.java
@@ -2,21 +2,25 @@ package br.com.alura.adopet.api.service;
 
 import br.com.alura.adopet.api.dto.adocao.SolicitacaoAdocaoDto;
 import br.com.alura.adopet.api.model.Abrigo;
+import br.com.alura.adopet.api.model.Adocao;
 import br.com.alura.adopet.api.model.Pet;
 import br.com.alura.adopet.api.model.Tutor;
 import br.com.alura.adopet.api.repository.AdocaoRepository;
 import br.com.alura.adopet.api.repository.PetRepository;
 import br.com.alura.adopet.api.repository.TutorRepository;
 import br.com.alura.adopet.api.validacoes.ValidacaoSolicitacaoAdocao;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
 
-import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 
 @ExtendWith(MockitoExtension.class)
@@ -37,7 +41,6 @@ class AdocaoServiceTest {
     @Mock
     private EmailService emailService;
 
-    @Mock
     private List<ValidacaoSolicitacaoAdocao> validacoes;
 
     @Mock
@@ -51,15 +54,26 @@ class AdocaoServiceTest {
 
     private SolicitacaoAdocaoDto dto;
 
+    @Captor
+    private ArgumentCaptor<Adocao> adocaoCaptor;
+
     @Test
     void deveriaSalvarAdocaoAoSolicitar() {
         // Arrange
+        this.dto = new SolicitacaoAdocaoDto(10l, 20l, "Quero adotar porque amo animais");
+        given(petRepository.getReferenceById(dto.idPet())).willReturn(pet);
+        given(tutorRepository.getReferenceById(dto.idTutor())).willReturn(tutor);
+        given(pet.getAbrigo()).willReturn(abrigo);
+
         // Act
         service.solicitar(dto);
 
         // Assert
-        then(petRepository).should().save(any()); // Verifica se o metodo save do repository foi chamado com o pet correto
-
+        then(repository).should().save(adocaoCaptor.capture()); // Verifica se o metodo save do repository foi chamado com o pet correto. Capturando o argumento passado para o metodo save.
+        Adocao adocaoSalva = adocaoCaptor.getValue();
+        Assertions.assertEquals(pet, adocaoSalva.getPet());
+        Assertions.assertEquals(tutor, adocaoSalva.getTutor());
+        Assertions.assertEquals(dto.motivo(), adocaoSalva.getMotivo());
     }
 
 }
