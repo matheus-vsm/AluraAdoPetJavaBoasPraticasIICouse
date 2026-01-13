80aaae0754634e912fa554b34683b94e2b97a463
matheus-vsm <scompinha@hotmail.com>
2026-01-12 23:29:09 -0300

    aula 5.2

---
+++ src/test/java/br/com/alura/adopet/api/controller/AdocaoControllerTest.java
@@ -0,0 +1,19 @@
+package br.com.alura.adopet.api.controller;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class AdocaoControllerTest {
+
+    @Test
+    void deveriaDevolverCodigo400ParaSolicitacaoDeAdocaoComErros() {
+        //ARRANGE
+        String json = "{}";
+
+        //ACT
+
+        //ASSERT
+        Assertions.assertEquals(400, ?);
+    }
+
+}
